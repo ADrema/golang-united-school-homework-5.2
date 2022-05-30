9c3f1e54ad97dbed266600e566be9cdf19cc8166
Anastasiia_Dremina <Anastasiia.Dremina@externals.adidas.com>
2022-05-30 21:22:28 +0300

    structure was modified

--- cache.go
+++ cache.go
@@ -2,21 +2,21 @@ package cache
 
 import "time"
 
-type Cache struct {
+type Element struct {
 	Key            string
 	Value          string
 	ExpirationDate time.Time
 }
 
-type CachesMap struct {
-	cacheMap []Cache
+type Cache struct {
+	cacheMap []Element
 }
 
 func NewCache() Cache {
 	return Cache{}
 }
 
-func (c *CachesMap) Get(key string) (string, bool) {
+func (c *Cache) Get(key string) (string, bool) {
 	deadline := time.Now()
 	c.CleanCache(deadline)
 	for i := range c.cacheMap {
@@ -27,7 +27,7 @@ func (c *CachesMap) Get(key string) (string, bool) {
 	return "", false
 }
 
-func (c *CachesMap) Put(key, value string) {
+func (c *Cache) Put(key, value string) {
 	deadline := time.Now()
 	c.CleanCache(deadline)
 	infiniteDate := time.Unix(1<<63-62135596801, 999999999)
@@ -41,12 +41,12 @@ func (c *CachesMap) Put(key, value string) {
 	}
 
 	if !exists {
-		c.cacheMap = append(c.cacheMap, Cache{key, value, infiniteDate})
+		c.cacheMap = append(c.cacheMap, Element{key, value, infiniteDate})
 	}
 
 }
 
-func (c *CachesMap) Keys() []string {
+func (c *Cache) Keys() []string {
 	var keys []string
 	for i := range c.cacheMap {
 		keys = append(keys, c.cacheMap[i].Key)
@@ -54,7 +54,7 @@ func (c *CachesMap) Keys() []string {
 	return keys
 }
 
-func (c *CachesMap) PutTill(key, value string, deadline time.Time) {
+func (c *Cache) PutTill(key, value string, deadline time.Time) {
 	currentTime := time.Now()
 	c.CleanCache(currentTime)
 	if deadline.After(currentTime) {
@@ -68,13 +68,13 @@ func (c *CachesMap) PutTill(key, value string, deadline time.Time) {
 		}
 
 		if !exists {
-			c.cacheMap = append(c.cacheMap, Cache{key, value, deadline})
+			c.cacheMap = append(c.cacheMap, Element{key, value, deadline})
 		}
 	}
 }
 
-func (c *CachesMap) CleanCache(deadline time.Time) {
-	var cleanedMap CachesMap
+func (c *Cache) CleanCache(deadline time.Time) {
+	var cleanedMap Cache
 	for i := range c.cacheMap {
 		if c.cacheMap[i].ExpirationDate.After(deadline) {
 			cleanedMap.cacheMap = append(cleanedMap.cacheMap, c.cacheMap[i])
